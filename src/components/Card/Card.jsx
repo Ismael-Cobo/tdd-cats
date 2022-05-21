00173821cf64d7062c76b9625325584e5dcf4f38
Ismael Cobo <midismael@gmail.com>
2022-05-21 18:57:57 +0200

    feat: Card component and styles added

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+
+import filledHeart from '../../svg/filledHeart.svg'
+import outlinedHeart from '../../svg/outlinedHeart.svg'
+import './card.css'
+
+export const Card = ({
+  name,
+  email,
+  phone,
+  image,
+  fav
+}) => {
+
+  const [isFavoured, setIsFavoured] = useState(fav)
+
+  const handleClick = () => {
+    setIsFavoured(!isFavoured)
+  }
+
+  return (
+    <div className='card'>
+      <div className="card-header">
+        <img src={image.url} alt={image.alt} className='card-img' aria-label={name} />
+        <button
+          className='heart'
+          onClick={handleClick}
+          lang={fav}
+        >
+          <img
+            src={isFavoured ? filledHeart : outlinedHeart}
+            alt={`${isFavoured ? 'filled heart' : 'outlined heart'}`} />
+        </button>
+      </div>
+      <div className="card-content">
+        <h3>{name}</h3>
+        <p>{phone}</p>
+        <p>{email}</p>
+      </div>
+    </div>
+  )
+}
